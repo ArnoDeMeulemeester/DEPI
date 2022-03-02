cc2e8ff1d5cb8bf9b2364ad88f6a719f80eb7dcc
Arvid Van Dorpe <Arvid.vandorpe@student.hogent.be>
2022-03-02 15:25:52 +0100

    Update SeleniumTest.py
    if it works it works

--- SeleniumTest.py
+++ SeleniumTest.py
@@ -11,19 +11,26 @@ nummer = input()
 
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
- 
-driver.get("https://cri.nbb.be/bc9/web/catalog?execution=e1s1")
 
-search = driver.find_element_by_id(
-    "page_searchForm:j_id3:generated_number_2_component")
-search.send_keys(nummer)
-search.send_keys(Keys.RETURN)
+driver.get("https://cri.nbb.be/bc9/web/catalog?execution=e2s1")
+
+try:
+    element1 = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located(
+            (By.ID, "page_searchForm:j_id3:generated_number_2_component"))
+    )
+    search = driver.find_element_by_id(
+        "page_searchForm:j_id3:generated_number_2_component")
+    search.send_keys(nummer)
+    search.send_keys(Keys.ENTER)
+finally:
+    print("hello")
 
 try:
     element = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.LINK_TEXT, "Download"))
     )
     element.click()
-except:
+finally:
+    time.sleep(5)
     driver.quit()
-
