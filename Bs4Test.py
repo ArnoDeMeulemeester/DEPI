7e3ce3228f3905abbf43ef2244252ad3111fae0b
Arno De Meulemeester <arno.demeulemeester@student.hogent.be>
2022-05-20 13:41:39 +0200

    scraper

--- Bs4Test.py
+++ Bs4Test.py
@@ -1,7 +1,10 @@
 # Testscenario van de tutorial
 
 from ast import Try
+from logging import exception
+from operator import truediv
 from re import T
+from time import sleep
 import requests
 import urllib.request
 from bs4 import BeautifulSoup
@@ -16,28 +19,37 @@ wbwritesheet = wbwrite.active
 r = 1
 c = 1
 
+def isNumber(x):
+  if x.isdigit():
+    return True
+  else:
+    return False
+
 for row in ws.rows:
     naam = row[1].value
     print(f"{r}. {naam}")
     naam = naam.replace(" ", "")
 
-
 # url site
     try:
-      url = 'https://www.bing.com/search?q=' + naam + "+website+Belgie"
-      res = requests.get(url)
-      # Connect to URL
-      html = requests.get(url, headers={"User-Agent": "Requests"})
-      obj = BeautifulSoup(html.text, "html.parser")
-      link = obj.findAll('cite')[0].get_text()
-      print(link)
-      print("--------------------------------------")
+      if(isNumber(naam)):
+        raise Exception("De naam van de kmo is een getal")
+      else:
+        url = 'https://www.bing.com/search?q=' + naam + "+website+Belgie"
+        res = requests.get(url)
+        # Connect to URL
+        html = requests.get(url, headers={"User-Agent": "Requests"})
+        obj = BeautifulSoup(html.text, "html.parser")
+        link = obj.findAll('cite')[0].get_text()
+        print(link)
+        print("--------------------------------------")
 
-      wbwritesheet.cell(row=r, column=c).value = link
-      # wbwrite.save("C:/Users/arvid/Documents/xcel/test.xlsx")
-      wbwrite.save("C:/Users/arnod/Documents/HoGent/wbwrite.xlsx")
-      r += 1
+        wbwritesheet.cell(row=r, column=c).value = link
+        # wbwrite.save("C:/Users/arvid/Documents/xcel/test.xlsx")
+        wbwrite.save("C:/Users/arnod/Documents/HoGent/wbwrite.xlsx")
+        r += 1
     except Exception as e:
       print(e)
       wbwritesheet.cell(row=r, column=c).value = "website niet gevonden"
+      wbwrite.save("C:/Users/arnod/Documents/HoGent/wbwrite.xlsx")
       r += 1
